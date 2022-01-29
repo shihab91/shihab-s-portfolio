6e1610706938ece11f42c02b1feafacf5543f437
shihab shumon <shumonshihab62@gmail.com>
2022-01-29 17:48:06 +0600

    made some components and started working with nav-menu

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
-import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import NavMenu from './components/NavMenu';
-import About from './pages/About';
-import Contact from './pages/Contact';
-import Home from './pages/Home';
-import Projects from './pages/Projects';
+/* eslint-disable quotes */
+/* eslint-disable prettier/prettier */
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import NavMenu from "./components/NavMenu";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
 
 export default function App() {
   return (
     <>
       <Router>
         <NavMenu />
-        <Home />
-        <About />
-        <Projects />
-        <Contact />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/home">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+          <Route path="/projects">
+            <Projects />
+          </Route>
+        </Switch>
       </Router>
     </>
   );
