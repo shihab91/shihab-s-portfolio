f29b102de5497bd49a346b5010b52eaa01454ed1
shihab shumon <shumonshihab62@gmail.com>
2022-02-01 16:06:04 +0600

    finished the contact section

---
+++ src/components/ContactForm.js
@@ -0,0 +1,83 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+/* eslint-disable react/no-unknown-property */
+/* eslint-disable quotes */
+/* eslint-disable prettier/prettier */
+import React, { useState } from "react";
+import styled from "styled-components";
+
+const ContactFormStyle = styled.div`
+  width: 100%;
+  .form__group {
+    width: 100%;
+    margin-bottom: 2rem;
+  }
+  label {
+    font-size: 1.8rem;
+  }
+  input,
+  textarea {
+    width: 100%;
+    font-size: 1.2rem;
+    padding: 1.2rem;
+    color: var(--grey-1);
+    background-color: var(--deep-dark);
+    outline: none;
+    border: none;
+    border-radius: 8px;
+    margin-top: 1rem;
+  }
+  textarea {
+    min-height: 250px;
+    resize: vertical;
+  }
+  button[type="submit"] {
+    background-color: var(--grey-1);
+    color: var(--black);
+    font-size: 2rem;
+    display: inline-block;
+    outline: none;
+    border: none;
+    padding: 1rem 4rem;
+    border-radius: 8px;
+    cursor: pointer;
+  }
+`;
+export default function ContactForm() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  return (
+    <ContactFormStyle>
+      <form>
+        <div className="form__group">
+          <label for="name">Your name</label>
+          <input
+            type="text"
+            id="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div className="form__group">
+          <label for="email">Your email</label>
+          <input
+            type="text"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="form__group">
+          <label for="message">Your message</label>
+          <textarea
+            type="text"
+            id="message"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
+        </div>
+        <button type="submit">Send</button>
+      </form>
+    </ContactFormStyle>
+  );
+}
